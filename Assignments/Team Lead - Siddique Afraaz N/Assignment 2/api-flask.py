58a10d5d1d8370f21d1b02c27bda0772694aa2fc
Siddiq Afraaz <63225471+SiddiqAfraaz@users.noreply.github.com>
2022-11-13 21:24:23 +0530

    Modify

--- Assignments/Team Lead - Siddique Afraaz N/Assignment 2/api-flask.py
+++ Assignments/Team Lead - Siddique Afraaz N/Assignment 2/api-flask.py
@@ -1,37 +1,4 @@
 from flask import Flask, render_template
-import ibm_db
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-import os
-from dotenv import load_dotenv
-from pathlib import Path
-
-dotenv_path = Path('secrets.env')
-load_dotenv(dotenv_path=dotenv_path)
-try: 
-    conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=0c77d6f2-5da9-48a9-81f8-86b520b87518.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31198;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID="+os.getenv("DBUSERNAME")+";PWD="+os.getenv("DBPASSWORD"),'','')
-    print("Connected to IBM DB2, Server Details:")
-    server = ibm_db.server_info(conn)
-
-    print ("DBMS_NAME: ", server.DBMS_NAME)
-    print ("DBMS_VER:  ", server.DBMS_VER)
-    print ("DB_NAME:   ", server.DB_NAME)
-except:
-    print("Error: Cannot connect with IBM DB2")
-
-message = Mail(
-    from_email='siddiqafraaz@gmail.com',
-    to_emails='2019ec0181@svce.ac.in',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(os.getenv('SENDGRIDAPIKEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
 
 app = Flask(__name__)
 
